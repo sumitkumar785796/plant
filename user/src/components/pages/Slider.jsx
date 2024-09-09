ceed0b3b4bfdec3a1780ed1826d2bba76e8bd5f9
sumitkumar785796 <76932212+sumitkumar785796@users.noreply.github.com>
2024-09-09 18:29:08 +0530

    somenew feature added

--- user/src/components/pages/Slider.jsx
+++ user/src/components/pages/Slider.jsx
@@ -8,23 +8,22 @@ const Slider = () => {
                 <div className="carousel-indicators">
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to={0} className="active" aria-current="true" aria-label="Slide 1" />
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to={1} aria-label="Slide 2" />
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to={2} aria-label="Slide 3" />
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to={3} aria-label="Slide 4" />
                 </div>
                 <div className="carousel-inner">
                     <div className="carousel-item active">
                         <img src="banner1.jpg" className="d-block w-100 img-fluid" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h2 style={{color:"yellowgreen"}}>Manavata Nursery</h2>
-                            <p style={{ color: 'white' }}>Manavata Nursery specializes in cultivating and selling a wide variety of plants, providing essential resources and expertise for gardening and landscaping...</p>
-
-                        </div>
+                        
                     </div>
                     <div className="carousel-item">
-                        <img src="banner2.jpg" className="d-block w-100 img-fluid" alt="..." />
-                        <div className="carousel-caption d-none d-md-block">
-                            <h2 style={{color:"yellowgreen"}}>Manavata Nursery</h2>
-                            <p style={{ color: 'white' }}>Manavata Nursery specializes in cultivating and selling a wide variety of plants, providing essential resources and expertise for gardening and landscaping...</p>
-                            
-                        </div>
+                        <img src="banner2.png" className="d-block w-100 img-fluid" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="banner3.png" className="d-block w-100 img-fluid" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="banner4.png" className="d-block w-100 img-fluid" alt="..." />
                     </div>
                 </div>
                 <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-bs-slide="prev">
